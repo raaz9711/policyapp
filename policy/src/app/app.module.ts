87d4f08b3a6f9924bef8906a94cd99c797f6cf0a
raaz9711 <raaz.kumar015@gmail.com>
2019-02-27 14:04:01 +0530

    frontpage

--- policy/src/app/app.module.ts
+++ policy/src/app/app.module.ts
@@ -3,10 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavComponent } from './nav/nav.component';
+import { LifeinsuranceComponent } from './lifeinsurance/lifeinsurance.component';
+import { HealthInsuranceComponent } from './health-insurance/health-insurance.component';
+import { MotorComponent } from './motor/motor.component';
+import { AboutComponent } from './about/about.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,
+    LifeinsuranceComponent,
+    HealthInsuranceComponent,
+    MotorComponent,
+    AboutComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
